ac2647452e4bebb4c494d638986b7cb479d080c9
stevelimly <49090381+stevelimly@users.noreply.github.com>
2022-11-07 23:23:02 +0800

    change variable to props

--- src/calculateWinner.js
+++ src/calculateWinner.js
@@ -1,4 +1,4 @@
-function calculateWinner(squares) {
+function calculateWinner(props) {
     const lines = [
         [0, 1, 2],
         [3, 4, 5],
@@ -11,8 +11,8 @@ function calculateWinner(squares) {
     ];
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+        if (props[a] && props[a] === props[b] && props[a] === props[c]) {
+            return props[a];
         }
     }
     return null;
